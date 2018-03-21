a54f06b330bf29ea8fc9fb2667feceef2934b2fa
BOUHAFSADMI <sadmibouhafs@gmail.com>
2018-03-21 15:19:48 +0100

    setted angular material

---
+++ src/app/material.module.ts
@@ -0,0 +1,57 @@
+import {NgModule} from '@angular/core';
+import {
+  MatInputModule,
+  MatGridListModule,
+  MatTabsModule,
+  MatButtonModule,
+  MatCheckboxModule,
+  MatSidenavModule,
+  MatToolbarModule,
+  MatCardModule,
+  MatSelectModule,
+  MatIconModule,
+  MatDialogModule,
+  MatSnackBarModule,
+  MatTooltipModule,
+  MatListModule
+} from '@angular/material';
+
+
+@NgModule({
+  imports: [
+    MatButtonModule,
+    MatCheckboxModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatCardModule,
+    MatTabsModule,
+    MatGridListModule,
+    MatInputModule,
+    MatSelectModule,
+    MatIconModule,
+    MatDialogModule,
+    MatSnackBarModule,
+    MatTooltipModule,
+    MatListModule
+  ],
+  exports: [
+    MatButtonModule,
+    MatCheckboxModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatCardModule,
+    MatTabsModule,
+    MatGridListModule,
+    MatInputModule,
+    MatSelectModule,
+    MatIconModule,
+    MatDialogModule,
+    MatSnackBarModule,
+    MatTooltipModule,
+    MatListModule
+  ],
+})
+
+export class MaterialModule { }
