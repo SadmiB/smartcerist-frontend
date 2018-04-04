7cdb7178fdc176d5a5b6fc775f3780c1993a73e6
BOUHAFSADMI <sadmibouhafs@gmail.com>
2018-04-04 22:56:27 +0100

    added header to dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,7 +19,9 @@ import { ServersComponent } from './components/servers/servers.component';
 import { RoomsComponent } from './components/rooms/rooms.component';
 import { NotificationsComponent } from './components/notifications/notifications.component';
 import { SettingsComponent } from './components/settings/settings.component';
-import { AnalyticsComponent } from './analytics/analytics.component';
+import { AnalyticsComponent } from './components/analytics/analytics.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HistoryComponent } from './history/history.component';
 
 
 
@@ -38,6 +40,8 @@ import { AnalyticsComponent } from './analytics/analytics.component';
     NotificationsComponent,
     SettingsComponent,
     AnalyticsComponent,
+    HeaderComponent,
+    HistoryComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent implements OnInit {
+
+  @Input() title: string;
+  @Input() search: boolean;
+
+  constructor(private auth: AuthService) { }
+
+  ngOnInit() {
+  }
+
+}
