e5076dc1d417c2f1da876f70fe71262912470bad
NAIT BELAID Mohand Ouamer <dm_nait_belaid@esi.dz>
2018-04-10 14:57:25 +0100

    complete the user service

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -17,6 +17,20 @@ export class UserService {
   }
 
   getUser(tokenHeader,userId){
-    return this.httpClient.get(this.BASE_URL + '/users/'+ userId , {headers:tokenHeader});
+    return this.httpClient.get(this.BASE_URL + '/users/user' , {headers:tokenHeader});
   }
+  
+  updateUser(tokenHeader,userId){
+    return this.httpClient.put(this.BASE_URL + '/users/user' , {headers:tokenHeader});
+  }
+  
+  removeUser(tokenHeader,userId){
+    return this.httpClient.delete(this.BASE_URL + '/users/user' , {headers:tokenHeader});
+  }
+
+  addUser(tokenHeader,userId){
+    return this.httpClient.post(this.BASE_URL + '/users', {headers:tokenHeader});
+  }
+
+  
 }
