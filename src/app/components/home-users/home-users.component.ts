a89d1bad952301f5b530e45cee2d9a5f3e2fcdd3
NAIT BELAID Mohand Ouamer <dm_nait_belaid@esi.dz>
2018-04-17 09:33:37 +0100

    add home users component

---
+++ src/app/components/home-users/home-users.component.ts
@@ -0,0 +1,146 @@
+import { Component, OnInit, Input, ViewChild } from '@angular/core';
+import { UserService } from '../../services/user.service';
+import { MatSnackBar, MatPaginator, MatSort, MatTableDataSource } from '@angular/material';
+import { SelectionModel } from '@angular/cdk/collections';
+import { AuthService } from '../../services/auth.service';
+import { SweetAlertService } from 'angular-sweetalert-service/js';
+
+@Component({
+  selector: 'app-home-users',
+  templateUrl: './home-users.component.html',
+  styleUrls: ['./home-users.component.scss']
+})
+export class HomeUsersComponent implements OnInit {
+  @Input() homeId:String;
+  tokenHeader;
+  displayedColumns = ['select', 'email','firstName', 'lastName','manage'];
+  dataSource ;
+  selection = new SelectionModel<Element>(true,[]);
+  
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+  
+
+  constructor(private userService:UserService,
+    private snackBar:MatSnackBar,
+    private auth:AuthService,
+    private alertService:SweetAlertService) { 
+      this.tokenHeader=auth.tokenHeader;
+    }
+
+  ngOnInit() {
+    this.getUsersHome()
+  }
+
+  private handleError(error, message) {
+    console.error(error);
+    this.snackBar.open(message, 'close', {duration: 3000});
+  }
+
+  getUsersHome() {
+    this.userService.getHomeUsers(this.tokenHeader,this.homeId)
+    .subscribe( (res:any) => {
+      this.dataSource = new MatTableDataSource(res);
+    }, error => {
+      this.handleError(error, 'Unable to retrieve users!');
+    });  
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
+  getUsersRoom(homeId,roomId) {
+    this.userService.getRoomUsers(this.tokenHeader,homeId,roomId)
+    .subscribe( (res:any) => {
+      this.dataSource = new MatTableDataSource(res);
+    }, error => {
+      this.handleError(error, 'Unable to retrieve users!');
+    });  
+    // this.dataSource.forEach(element => {
+    //   element.permission=this.getUserRoomPermission(homeId,roomId,element._id);       
+    // });
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
+  // getUserRoomPermission(homeId,roomId,userId){
+  //   this.roomService.getUserRoomPermission(homeId,roomId,userId,this.tokenHeader)
+  //   .subscribe(res=> {
+  //     res;
+  //   },error => {
+  //     this.handleError(error,'Unable to retrieve user permission for this room');
+  //   })
+  // }
+
+  updateUserPermission(){}
+  /** Whether the number of selected elements matches the total number of rows. */
+isAllSelected() {
+  const numSelected = this.selection.selected.length;
+  const numRows = this.dataSource.data.length;
+  return numSelected == numRows;
+}
+
+/** Selects all rows if they are not all selected; otherwise clear selection. */
+masterToggle() {
+  this.isAllSelected() ?
+      this.selection.clear() :
+      this.dataSource.data.forEach(row => this.selection.select(row));
+}
+
+  applyFilter(filterValue: string) {
+    filterValue = filterValue.trim(); // Remove whitespace
+    filterValue = filterValue.toLowerCase(); // MatTableDataSource defaults to lowercase matches
+    this.dataSource.filter = filterValue;
+  }
+
+  deleteUser(userId){
+    const options = {
+      title: 'Are you sure?',
+      text: "You won't be able to revert this!",
+      type: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Yes, delete it!'
+    };
+    this.alertService.confirm(options)
+    .then(() => {
+      this.userService.removeUser(this.tokenHeader,userId)
+      .subscribe(res => {
+        res;
+        this.alertService.success({
+          title: 'Account deleted'
+        });
+      } ,error =>{
+        this.handleError(error,'Unable to remove User')
+      })
+    })
+    .catch(() => console.log('canceled')); 
+  }
+  
+  editUser(userId){
+    const options = {
+      title: 'Permission',
+      type: 'info',
+      html:
+      `<form action="">
+        <label >User
+          <input type="radio" checked="checked" >
+        </label>
+        <label >Admin
+          <input type="radio" name="radio" value="Admin">
+        </label>
+      </form>`,
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'save'
+    };
+    this.alertService.confirm(options)
+  }
+
+}
+export interface Element {
+  firstName: string;
+  lastName: string;
+  email: string;
+}
