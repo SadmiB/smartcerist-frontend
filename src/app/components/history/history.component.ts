11ea6ded5558c9143da98085b68c9201ec19deaf
Tapa Autiomaan <dm_nait_belaid@esi.dz>
2018-05-24 00:23:44 +0100

    - add a content for the  history component - change home, room, ObjEvent models - edit homesService and eventsService to load the events from the database

--- src/app/components/history/history.component.ts
+++ src/app/components/history/history.component.ts
@@ -9,6 +9,7 @@ import 'rxjs/add/operator/skipWhile';
 import 'rxjs/add/operator/scan';
 import 'rxjs/add/operator/throttleTime';
 import { AuthService } from '../../services/auth.service';
+import { HomesService } from '../../services/homes.service';
 
 @Component({
   selector: 'app-history',
@@ -16,43 +17,16 @@ import { AuthService } from '../../services/auth.service';
   styleUrls: ['./history.component.scss']
 })
 export class HistoryComponent implements OnInit {
-  message: string;
-  messages: string[] = [];
-  secretCode: string;
+
   tokenHeader;
 
   constructor(private auth: AuthService,
     private snackBar: MatSnackBar,
-    protected chatService: EventsService) {
-    this.secretCode = 'DONT';
+    protected homesService: HomesService) {
     this.tokenHeader = auth.tokenHeader;
   }
 
-  sendMessage() {
-    this.snackBar.open('hello friends how are you', '', {
-      duration: 5000,
-      horizontalPosition: 'right',
-      verticalPosition: 'bottom',
-      extraClasses: ['danger']
-    });
-    this.chatService.sendMessage(this.message);
-    this.message = '';
-  }
-
   ngOnInit() {
-    this.chatService.getSocketRooms(this.tokenHeader);
-    this.chatService
-      .getMessages()
-      .distinctUntilChanged()
-      .filter((message) => message.trim().length > 0)
-      .throttleTime(1000)
-      .scan((acc: string, message: string, index: number) =>
-          `${message}(${index + 1})`
-        , 1)
-      .subscribe((message: string) => {
-        const currentTime = moment().format('hh:mm:ss a');
-        const messageWithTimestamp = `${currentTime}: ${message}`;
-        this.messages.push(messageWithTimestamp);
-      });
+    this.homesService.getOwnerHomes(this.tokenHeader);
   }
 }
--- src/app/models/Home.ts
+++ src/app/models/Home.ts
@@ -1,15 +1,16 @@
 import {Room} from './Room';
-export class Home{
-    _id: String;
-    name: String;
-    address:  String;
-    email: String;
-    telephone: String;
-    country:  String;
-    long: String;
-    lat: String;
-    picture: String;
-    owner: String;
-    servers: String[];
+export class Home {
+    _id: string;
+    name: string;
+    address:  string;
+    email: string;
+    telephone: string;
+    country:  string;
+    long: string;
+    lat: string;
+    picture: string;
+    owner: string;
+    servers: string[];
     rooms: Room[];
+    permission: string;
 }
